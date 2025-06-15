c88da86d220501d912eddc50f1aa1cdf0724fb9a
uhcfn <uhcfn.fn@gmail.com>
2025-06-15 23:53:00 +0600

    Update packages.py

--- Builder/packages.py
+++ Builder/packages.py
@@ -20,16 +20,16 @@ DEV_PACKAGES = [
     "cheese", "screenkey", "timeshift", "pinta", "kdenlive", "wireshark-qt",
     "filezilla", "ghex", "chromium", "keepassxc", "audacity", "gufw", "python-pywal",
     "bleachbit", "veracrypt", "homebank", "gtkhash", "gnome-firmware", "touche", "dconf-editor",
-    "neovim", "obs-studio", "telegram-desktop", "tmux", "youtube-dl", "code", "cowsay",
-    "deluge-gtk", "flameshot", "sqlitebrowser", "obsidian", "python-pip", "bpython", "ipython", "cloc",
+    "neovim", "telegram-desktop", "code",
+    "flameshot", "obsidian", "python-pip", "bpython", "ipython"
 ]
 
 AUR_PACKAGES = [
-    "cava", "i3lock-color", "ptpython"
+    "cava", "i3lock-color"
 ]
 
 GNOME_OFFICIAL_TOOLS = [
-    "evince", "gnome-calculator", "gnome-disk-utility", "gucharmap",
-    "gthumb", "gnome-clocks"
+    "gnome-disk-utility",
+    "gthumb"
 ]
 
